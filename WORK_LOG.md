0557a89a82915bc5c43c166aff5978559d2093da
L-Tran <logan.tran@menloschool.org>
2025-08-27 22:59:52 -0700

    Coded puzzles 1-4

--- WORK_LOG.md
+++ WORK_LOG.md
@@ -5,6 +5,8 @@ Name: Logan Tran
 | Aug 24 | 7-7:30pm | Solved first and second puzzle with Lucas Huang using 9+ caesar cypher and ascii table - both lyrics of Lewis Carroll after quick google search |
 | Aug 24 | 7:30-8pm |                                                     Solved third puzzle using binary to letters with Lucas Huang - also lyrics to Lewis Carroll |
 | Aug 26 | 9-9:45pm |                                                                                          Tried to solve last puzzle and started to look at code |
+| Aug 27 |  8-10pm  |                                                                                          Solved fourth puzzle and coded first and fourth puzzle |
+| Aug 27 | 10-11pm  |                                                                                              Coded third then second puzzle, and added comments |
 
 
 To add a new row to the table, click into a cell and then hit shift-enter.
--- src/Riddler.java
+++ src/Riddler.java
@@ -3,42 +3,87 @@
  * A puzzle by Zach Blick
  * for Adventures in Algorithms
  * At Menlo School in Atherton, CA
- *
  * Completed by: Logan Tran
  */
 public class Riddler {
 
+    // Static Variables for Shifts
+    public static int CAESAR_SHIFT = 9;
+    public static int DINGBAT_CAESAR_SHIFT = 9919;
+
+    // Caesar shift +9
     public String decryptOne(String encrypted) {
         String decrypted = "";
-
+        // Loop through encrypted message
         for(int i = 0; i < encrypted.length(); i++){
+            char c = encrypted.charAt(i);
+            // Find case of letter to get to proper ascii
+            if (Character.isUpperCase(c)) {
+                // Shift letter 9 then convert back to ascii
+                c = (char)((c + CAESAR_SHIFT - 'A') % 26 + 'A');
+            }
+            else if (Character.isLowerCase(c)){
+                // Shift letter 9 then convert back to ascii
+                c = (char)((c + CAESAR_SHIFT - 'a') % 26 + 'a');
+            }
+            decrypted += c;
+        }
+        return decrypted;
+    }
 
+    // Ascii conversion
+    public String decryptTwo(String encrypted) {
+        String decrypted = "";
+        // Split string into array of strings of ascii numbers
+        String[] ascii = encrypted.split(" ");
+        // Loop through strings
+        for(String s: ascii){
+            // Convert to Int then Char
+            char c = (char)Integer.parseInt(s);
+            decrypted += c;
+        }
+        return decrypted;
+    }
+
+    // Binary conversion
+    public String decryptThree(String encrypted) {
+        String decrypted = "";
+        // Loop through in group of 8
+        for(int i = 0; i < encrypted.length(); i+=8){
+            String numString = "";
+            // Loop through each group
+            for(int j = 0; j < 8; j++){
+                // Add each 1 or 0 to binary string
+                numString += encrypted.charAt(i+j);
+            }
+            // Convert binary string to int
+            int num = Integer.parseInt(numString);
+            int asciiNum = 0;
+            int counter = 0;
+            // Until binary int is 0
+            while(num > 0){
+                // Multiple and add number depending on position
+                asciiNum += (int) ((num % 10) * Math.pow(2,counter));
+                // Shorten number and addd to counter
+                num /= 10;
+                counter++;
+            }
+            decrypted += (char)asciiNum;
         }
 
         return decrypted;
     }
 
-    public String decryptTwo(String encrypted) {
-        String decrypted = "";
-
-        // TODO: Complete the decryptTwo() function.
-
-        return decrypted;
-    }
-
-    public String decryptThree(String encrypted) {
-        String decrypted = "";
-
-        // TODO: Complete the decryptThree() function.
-
-        return decrypted;
-    }
-
+    // Caesar Shift -9919 from Dingbat
     public String decryptFour(String encrypted) {
         String decrypted = "";
-
-        // TODO: Complete the decryptFour() function.
-
+        // Loop through encrypted message
+        for(int i = 0; i < encrypted.length(); i++){
+            // Shift every digit down
+            char c = encrypted.charAt(i);
+            c = (char)(c - DINGBAT_CAESAR_SHIFT);
+            decrypted += c;
+        }
         return decrypted;
     }
 }
